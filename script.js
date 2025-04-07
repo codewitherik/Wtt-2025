aa72b788216e4e7694d09b6f1abec91eb6cb3a93
Codewitherik <122739986+codewitherik@users.noreply.github.com>
2025-04-07 10:42:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const spiele = [
+    {
+        zeit: "09:00",
+        teamA: "Ice Kings",
+        teamB: "Czech Stars",
+        halle: "Halle 1",
+        gruppe: "A",
+        ergebnis: "3 : 1"
+    },
+    {
+        zeit: "09:30",
+        teamA: "Budapest U10",
+        teamB: "Berlin Kids",
+        halle: "Halle 2",
+        gruppe: "B",
+        ergebnis: "-"
+    }
+];
